118e1dd4940081c60a657a242b7e4ad562c27598
meghananelapati <119791390+meghananelapati@users.noreply.github.com>
2024-01-08 21:00:40 +0530

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,31 +1,3 @@
-// const mongoose=require('mongoose');
-// const Schema=mongoose.Schema;
-
-// const blogSchema=new Schema({
-//     title:{
-//         type:String,
-//         required:true
-//     },
-//     snippet:{
-//         type:String,
-//         required:true
-//     },
-//     body: {
-//         type: String,
-//         required: true
-//     }
-// }, {timestamps: true});
-
-// const Blog=mongoose.model('Blog', blogSchema);
-// module.exports= blog;
-
-
-//IMPPP
-//views are where we make our HTML templates that a user will see
-//models are how we describe our data structure and we use them to interact with the database
-
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
